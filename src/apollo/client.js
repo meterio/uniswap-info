4b31ce40099f62f413813e92840c1d746954fd96
Simon Zhang <zhanghan.simon@gmail.com>
2021-05-22 12:57:38 +0800

    first import

---
+++ src/apollo/client.js
@@ -0,0 +1,34 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://graphtest.meter.io/subgraphs/name/meterio/uniswap-v2-subgraph'
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true
+})
+
+export const healthClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://graphhealth.meter.io'
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true
+})
+
+export const v1Client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap'
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true
+})
+
+export const blockClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://graphtest.meter.io/subgraphs/name/blocklytics/ethereum-blocks'
+  }),
+  cache: new InMemoryCache()
+})
