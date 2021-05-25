0ddd457197c493fa3b7ad71a1ff3502c9e81c3f7
Simon Zhang <zhanghan.simon@gmail.com>
2021-05-25 11:39:26 +0800

    update graphhealth url

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -12,7 +12,7 @@ export const client = new ApolloClient({
 
 export const healthClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://graphhealth.meter.io'
+    uri: 'https://graphhealth.meter.io/graphql'
   }),
   cache: new InMemoryCache(),
   shouldBatch: true
