a48b9fd6503fe46c09a294872fd9888420a776be
Simon Zhang <zhanghan.simon@gmail.com>
2021-11-27 15:03:34 -0800

    update deploy script; update graph url

--- deploy_mainnet.sh
+++ deploy_mainnet.sh
@@ -4,8 +4,7 @@
 NODE_ENV=production yarn build
 
 # copy dist to mainnet S3
-aws s3 sync ./build/ s3://voltswap-info-main/ --delete --acl public-read
+aws s3 sync ./build/ s3://info.voltswap.finance/ --delete --acl public-read
 
 # invalidate mainnet cloudfront cache
-aws cloudfront create-invalidation --distribution-id E122HAI06IXNWD --paths "/*"
-
+aws cloudfront create-invalidation --distribution-id E1H47QPYGN569S --paths "/*"
