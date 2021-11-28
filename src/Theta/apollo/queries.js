9559dee1c6da6100fe1311097275005edd25ab05
philipappiah <pappiahkubi1@gmail.com>
2021-11-28 00:26:49 +0000

    fix theta graph health query

--- src/Theta/apollo/queries.js
+++ src/Theta/apollo/queries.js
@@ -3,7 +3,7 @@ import { FACTORY_ADDRESS, BUNDLE_ID } from '../constants'
 
 export const SUBGRAPH_HEALTH = gql`
   query health {
-    indexingStatusForCurrentVersion(subgraphName: "meterio/uniswap-v2-subgraph") {
+    indexingStatusForCurrentVersion(subgraphName: "theta/uniswap-v2-subgraph") {
       synced
       health
       chains {
