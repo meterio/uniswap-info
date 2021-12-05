45ed37416a345b0bf89c7d9d0114818a09e5416f
philipappiah <pappiahkubi1@gmail.com>
2021-12-05 17:21:57 +0000

    change name theta to tfuel

--- src/Theta/components/GlobalStats/index.js
+++ src/Theta/components/GlobalStats/index.js
@@ -47,7 +47,7 @@ export default function GlobalStats() {
               }}
               style={{ position: 'relative' }}
             >
-              Theta Price: <Medium>{formattedEthPrice}</Medium>
+              TFuel Price: <Medium>{formattedEthPrice}</Medium>
               {showPriceCard && <UniPrice />}
             </TYPE.main>
           )}
